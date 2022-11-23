29eb7c3c5bfb51d270bdd5ffd4f57e5a996ed90c
JedsadaSrijunpoe <tae456852@gmail.com>
2022-11-23 21:46:06 +0700

    Update ir_reciever_test.ino

--- ir_reciever_test/ir_reciever_test.ino
+++ ir_reciever_test/ir_reciever_test.ino
@@ -1,44 +1,23 @@
 #include <IRremote.hpp>
 
 #define IR_RECEIVE_PIN 5
-#define led1 22
-
-#define code1 0xE916FF00
-#define code2 0xB24DFF00
-
-#define LED_OFF HIGH // active-low
-#define LED_ON LOW
 
 void setup()
 {
   Serial.begin(115200);
   IrReceiver.begin(IR_RECEIVE_PIN); // Start the receiver
-  pinMode(led1, OUTPUT);
-  digitalWrite(led1, LED_OFF);
 }
 
 void loop() {
   if (IrReceiver.decode()) {
-      //int result = IrReceiver.decodedIRData.decodedRawData;
-      Serial.print("Code: ");
-      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-      
-      IrReceiver.printIRResultRawFormatted(&Serial, true);
-      //Serial.println("end_raw_data");
-      Serial.write(13);
-      Serial.write(10);
 
-      //IrReceiver.printIRResultRawFormatted(&Serial, true);
-      //IrReceiver.printIRResultShort(&Serial); // optional use new print version
+      Serial.print("Code: ");
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Decoded to HEX
       
-      // if (result == code1) { // ถ้าค่าที่ส่งมาตรงกับ ค่าในตัวแปร code1 ให้ทำใน ปีกกา if
-      //   Serial.println("LED ON");
-      //   digitalWrite(led1, LED_ON);
-      // }
-      // if (result == code2) { // ถ้าค่าที่ส่งมาตรงกับ ค่าในตัวแปร code2 ให้ทำใน ปีกกา if
-      //   Serial.println("LED OFF");
-      //   digitalWrite(led1, LED_OFF);
-      // }
+      IrReceiver.printIRResultRawFormatted(&Serial, true); // Print raw timing data
+
+      Serial.write(13); // CR
+      Serial.write(10); // LF
 
       IrReceiver.resume(); // Enable receiving of the next value
   }
